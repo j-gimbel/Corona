9f393ef6f7434607ee076e5339f4788e3f9e0f95
Jan Gimbel <github-4597@jgimbel.de>
2021-03-09 23:13:05 +0100

    made it work on windows

--- convertARD.py
+++ convertARD.py
@@ -9,9 +9,11 @@ from datetime import datetime, date
 import cov_dates as cd
 import json
 import ndjson
+import glob
 
 # cd /Users/pavel/Corona/2020-rki-archive/data/2_parsed
 # python ~/Corona/convertARD.py -d ~/Corona/archive_ard/ *.bz2
+
 
 def datetimeFromARDFilenameBZ2(ds):
     st = time.strptime(ds, "data_%Y-%m-%d-%H-%M.json.bz2")
@@ -19,44 +21,63 @@ def datetimeFromARDFilenameBZ2(ds):
     sdt = datetime.fromtimestamp(stf)
     return sdt
 
+
 def datetimeFromARDFilenameXZ(ds):
     st = time.strptime(ds, "data_%Y-%m-%d-%H-%M.ndjson.xz")
     stf = time.mktime(st)
     sdt = datetime.fromtimestamp(stf)
     return sdt
 
+
 def datetimeFromARDFilename(ds):
     if ds.endswith("xz"):
         return datetimeFromARDFilenameXZ(ds)
     return datetimeFromARDFilenameBZ2(ds)
 
-def convert(compressedJSONFile, destDir=".", force = False):
+
+def convert(compressedJSONFile, destDir=".", force=False):
     path = os.path.normpath(compressedJSONFile)
     fileName = path.split(os.sep)[-1]
     date = datetimeFromARDFilename(fileName)
     day = cd.dayFromDate(date)
-    newFile =  destDir+"/NPGEO-RKI-{}.csv".format(cd.dateStrYMDFromDay(day))
+    newFile = destDir + "/NPGEO-RKI-{}.csv".format(cd.dateStrYMDFromDay(day))
 
     if force or not os.path.isfile(newFile):
         print("Loading " + compressedJSONFile)
-        #with bz2.open(compressedJSONFile, "rb") as f:
+        # with bz2.open(compressedJSONFile, "rb") as f:
         with lzma.open(compressedJSONFile, "rb") as f:
             content = ndjson.load(f)
             frame = dt.Frame(content)
             print("Saving " + newFile)
             frame.to_csv(newFile)
     else:
-        print("Skipping '{}' because '{}' already exists".format(compressedJSONFile, newFile))
+        print(
+            "Skipping '{}' because '{}' already exists".format(
+                compressedJSONFile, newFile
+            )
+        )
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Convert ARD-RKI-dumps to .csv')
-    parser.add_argument('files', metavar='fileName', type=str, nargs='+',
-                        help='Convert ARD-RKI-dumps to .csv')
-    parser.add_argument('-d', '--output-dir', dest='outputDir', default=".")
+    parser = argparse.ArgumentParser(description="Convert ARD-RKI-dumps to .csv")
+    parser.add_argument(
+        "files",
+        metavar="fileName",
+        type=str,
+        nargs="+",
+        help="Convert ARD-RKI-dumps to .csv",
+    )
+    parser.add_argument("-d", "--output-dir", dest="outputDir", default=".")
     args = parser.parse_args()
     print(args)
-    for f in args.files:
+    files = args.files
+    if os.name == "nt":
+        files = glob.glob(args.files[0])
+    for f in files:
+        print(f)
+        # for f in glob.glob(args.files)
         convert(f, args.outputDir)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
